33e6905828265de3565476951dd7bc5ce52f6fbb
Nuno Silva <nuno.silva@bskyb.com>
2015-10-28 16:22:40 +0000

    Change server.js to support node version 0.x (removed => ES6 notation)

--- server.js
+++ server.js
@@ -11,11 +11,11 @@ var compiler = webpack(config)
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
 app.use(webpackHotMiddleware(compiler))
 
-app.get('/*', (req, res) => {
+app.get('/*', function (req, res) {
   res.sendFile(__dirname + '/index.html')
 })
 
-app.listen(port, error => {
+app.listen(port, function (error) {
   if (error) {
     console.error(error)
   } else {
