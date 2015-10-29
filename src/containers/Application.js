2f49d260bcfc38e0649a803ace872a874d5837a2
Carlos Guedes <cguedes@gmail.com>
2015-10-29 01:06:43 +0000

    Adds initial Application structure with ExplorerHeader component

---
+++ src/containers/Application.js
@@ -0,0 +1,61 @@
+import './Application.css'
+import React, { Component, PropTypes } from 'react'
+import { bindActionCreators } from 'redux'
+import { connect } from 'react-redux'
+import * as TodoActions from '../actions/todos'
+import { Link } from 'react-router'
+import { ExplorerHeader } from '../components'
+
+class Application extends Component {
+  render () {
+    const { todos, actions, addTodo } = this.props
+    return (
+      <div id='content'>
+        <div id='sidebar'>
+          <div className='api-explorer-logo'>
+            <Link to='/' >
+            <img src='https://dev.int.skystore.com/api/explorer/public/images/logo_transparent.png' />
+            </Link>
+          </div>
+          <ul className='nav' id='side-menu'>
+            <li>
+              <Link to='/' >
+              <i className='fa fa-fw fa-home'></i><span>Home</span>
+              </Link>
+            </li>
+          </ul>
+          <div id='lateral-menu'>
+            LATERAL MENU
+          </div>
+        </div>
+        <div id='main-content'>
+          <div className='container-fluid'>
+            <div className='row' id='top'>
+              <div className='col-lg-12'>
+                <ExplorerHeader api={{ apiName: 'Sample API', apiVersion: '1.0.0', productVersion: 'asdasd12' }} />
+                {this.props.children}
+                <div>FOOTER</div>
+                  Copyright &copy; API Explorer 2015
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
+Application.propTypes = {
+  todos: PropTypes.array.isRequired,
+  actions: PropTypes.object.isRequired,
+  addTodo: PropTypes.func.isRequired,
+  children: PropTypes.element
+}
+
+export default connect(
+  state => ({ todos: state.todos }),
+  dispatch => ({
+    addTodo: (...args) => dispatch(TodoActions.addTodo(...args)),
+    actions: bindActionCreators(TodoActions, dispatch)
+  })
+)(Application)
--- src/routes/index.js
+++ src/routes/index.js
@@ -1,16 +1,17 @@
 import React from 'react'
 import { Route } from 'react-router'
 
-import App from '../containers/App'
+//import App from '../containers/App'
+import Application from '../containers/Application'
 import OtherPage from '../containers/OtherPage'
 import Settings from '../containers/SettingsPage'
 
 export default function () {
   return (
     <Route>
-      <Route path='/' component={App} />
+      <Route path='/' component={Application} />
       <Route path='/other' component={OtherPage} />
       <Route path='/settings' component={Settings} />
-    </Route>
+    </Route> 
   )
 }
