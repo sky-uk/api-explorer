9a79d4b0c876b88989a4d36476eeb88bc5c32f8f
Carlos Guedes <cguedes@gmail.com>
2015-11-06 08:53:54 +0000

    Fix lint errors

--- sampleapp/index-sampleapp.js
+++ sampleapp/index-sampleapp.js
@@ -2,14 +2,14 @@ import APIExplorer from './../src'
 
 APIExplorer
   .config(c => {
-    //c.swagger2API('petshopx', 'https://raw.githubusercontent.com/swagger-api/swagger-spec/master/examples/v2.0/json/petstore-with-external-docs.json')
-    //c.swagger2API('petshop', 'https://api.swaggerhub.com/apis/anil614sagar/petStore/1.0.0')
+    // c.swagger2API('petshopx', 'https://raw.githubusercontent.com/swagger-api/swagger-spec/master/examples/v2.0/json/petstore-with-external-docs.json')
+    // c.swagger2API('petshop', 'https://api.swaggerhub.com/apis/anil614sagar/petStore/1.0.0')
     c.swagger2API('Pet Store', '/samples/petstore.json')
-    //c.swagger2API('Twitter', '/samples/twitter.json')
+    // c.swagger2API('Twitter', '/samples/twitter.json')
     c.swagger2API('Instagram', '/samples/instagram.json')
-    //c.swagger2API('petshopa', 'http://petstore.swagger.io/v2/swagger.json')
+    // c.swagger2API('petshopa', 'http://petstore.swagger.io/v2/swagger.json')
   })
   .configWidgetTabs(c => {
-    //c.addWidgetTab('HATEOAS', SSPAPIExplorer.HATEOASWidget)
+    // c.addWidgetTab('HATEOAS', SSPAPIExplorer.HATEOASWidget)
   })
   .start()
