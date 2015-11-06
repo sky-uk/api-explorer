b7c74c26b2dd9b6dc6ec4efd5dfca4a6d638ad36
Nuno Silva <nunos@zi-yu.com>
2015-11-06 09:39:58 +0000

    first version of http proxy

--- server.js
+++ server.js
@@ -26,3 +26,19 @@ app.listen(port, function (error) {
     console.info('==> 🌎  Listening on port %s. Open up http://localhost:%s/ in your browser.', port, port)
   }
 })
+
+var http = require('http'),
+    httpProxy = require('http-proxy')
+//
+// Create your proxy server and set the target in the options.
+//
+httpProxy.createProxyServer({target:'http://localhost:3000'}).listen(8000)
+
+//
+// Create your target server
+//
+http.createServer(function (req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' })
+  res.write('request successfully proxied!' + '\n' + JSON.stringify(req.headers, true, 2))
+  res.end()
+}).listen(9000)
