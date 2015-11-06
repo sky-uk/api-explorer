ae88204e0ccde8d3ccb04e23f8fef513004b1a58
Nuno Silva <nuno.silva@bskyb.com>
2015-11-06 15:18:23 +0000

    lint correction

--- sampleapp/index-sampleapp.js
+++ sampleapp/index-sampleapp.js
@@ -3,8 +3,8 @@ import APIExplorer from './../src'
 APIExplorer
   .config(c => {
     // c.swagger2API('petshopx', 'https://raw.githubusercontent.com/swagger-api/swagger-spec/master/examples/v2.0/json/petstore-with-external-docs.json')
-    // c.swagger2API('petshop', 'https://api.swaggerhub.com/apis/anil614sagar/petStore/1.0.0')
     c.swagger2API('petshop', 'https://api.swaggerhub.com/apis/anil614sagar/petStore/1.0.0', true)
+    // c.swagger2API('petshop', 'https://api.swaggerhub.com/apis/anil614sagar/petStore/1.0.0', true)
     // c.swagger2API('petshop', 'https://petstore.swagger.io/v2/swagger.json')
     // c.swagger2API('Pet Store', '/samples/petstore.json')
     // c.swagger2API('Twitter', '/samples/twitter.json')
--- server.js
+++ server.js
@@ -10,7 +10,6 @@ var Express = require('express')
 var app = new Express()
 var port = process.env.PORT || 3000
 
-
 var compiler = webpack(config)
 app.use(webpackDevMiddleware(compiler, { noInfo: true, publicPath: config.output.publicPath }))
 app.use(webpackHotMiddleware(compiler))
