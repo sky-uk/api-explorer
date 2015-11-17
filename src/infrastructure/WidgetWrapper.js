45c69260a943f185c5217118a0db275a3afb82a4
João Pinho <joao.pinho@sky.uk>
2015-11-17 17:10:55 +0000

    Refactored url protocol parsing into Url Object

--- src/infrastructure/WidgetWrapper.js
+++ src/infrastructure/WidgetWrapper.js
@@ -3,15 +3,16 @@ import { connect } from 'react-redux'
 export default function widgetWrapper (widgetTab) {
   return connect(
           state => {
-            const URI_PROTOCOL_REGEX = /([a-z]+):\/\//
             let operation = state.operations.filter(op => op.get('id') === state.router.params.id).first()
             operation = operation.size > 0 ? operation.toJS() : null
-            let defaultScheme = URI_PROTOCOL_REGEX.exec(state.configs.get('url').url).splice(1).toString()
             return {
               operation: operation,
               definitions: state.definitions.size > 0 ? state.definitions.toJS() : {},
               apis: state.apis.get('byName').get(operation.apiname),
-              config: { defaultScheme: defaultScheme, useProxy: state.configs.get('url').useProxy, headers: state.configs.get('headers') }
+              config: {
+                defaultScheme: state.configs.get('url').protocol,
+                useProxy: state.configs.get('url').useProxy,
+                headers: state.configs.get('headers') }
             }
           }
         )(widgetTab)
