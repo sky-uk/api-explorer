e398d73faab71b9dcad38db921cc5028c6becbaa
João Pinho <joao.pinho@sky.uk>
2015-11-17 16:53:59 +0000

    Refactored regex behind meaningful constant symbol

--- src/infrastructure/WidgetWrapper.js
+++ src/infrastructure/WidgetWrapper.js
@@ -3,9 +3,10 @@ import { connect } from 'react-redux'
 export default function widgetWrapper (widgetTab) {
   return connect(
           state => {
+            const URI_PROTOCOL_REGEX = /([a-z]+):\/\//
             let operation = state.operations.filter(op => op.get('id') === state.router.params.id).first()
             operation = operation.size > 0 ? operation.toJS() : null
-            let defaultScheme = /([a-z]+):\/\//.exec(state.configs.get('url').url).splice(1).toString()
+            let defaultScheme = URI_PROTOCOL_REGEX.exec(state.configs.get('url').url).splice(1).toString()
             return {
               operation: operation,
               definitions: state.definitions.size > 0 ? state.definitions.toJS() : {},
