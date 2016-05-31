525f00431f5cc809891e8322bf62f63f87d0327f
Tiago Sousa <tiago.sousa@sky.uk>
2016-05-31 16:32:48 +0100

    different methods response examples, with and without deprecated state

--- sampleapp/helloworld/api.js
+++ sampleapp/helloworld/api.js
@@ -6,4 +6,12 @@ module.exports = function (expressApp, port, basePath) {
   expressApp.get('/sampleapp/helloworld/hello', function (req, res) {
     res.json({response: 'Hello World'})
   })
+
+  expressApp.all('/sampleapp/helloworld/hello/*', function (req, res) {
+    res.json({response: req.path})
+  })
+
+  expressApp.all('/sampleapp/helloworld/oldhello*', function (req, res) {
+    res.json({response: 'Deprecated operation'})
+  })
 }
