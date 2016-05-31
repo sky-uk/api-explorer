3b4fbdf51135df570b69015d21f3824eb7e07026
Tiago Sousa <tiago.sousa@sky.uk>
2016-05-31 13:15:37 +0100

    environment variables

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,12 @@
 var path = require('path')
 var webpack = require('webpack')
-var sample = process.env.APIEXPLORER_SAMPLE.replace(/ /, '')
+var sample = process.env.APIEXPLORER_SAMPLE
 
 module.exports = {
   devtool: 'cheap-module-eval-source-map',
   entry: [
     'webpack-hot-middleware/client',
-    './sampleapp/' + sample + '/index.js'
+    './sampleapp/' + sample
   ],
   output: {
     path: path.join(__dirname, 'dist'),
