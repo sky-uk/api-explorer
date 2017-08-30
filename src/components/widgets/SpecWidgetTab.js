228bfb097dd3fa7d7862ecbafed4345c25e60a89
Daniel Correia <daniel.correia@sky.uk>
2017-08-30 11:21:43 +0100

    Add basic example support.

--- src/components/widgets/SpecWidgetTab.js
+++ src/components/widgets/SpecWidgetTab.js
@@ -1,20 +1,31 @@
 import React, { Component, PropTypes } from 'react'
 
 class SpecWidgetTab extends Component {
+
+  renderParameterDescription (description) {
+    return description.startsWith('http')
+      ? (<a href={description} target='_blank'>Go to external docs <i className='fa fa-external-link' /></a>)
+      : (<div>{description}</div>)
+  }
+
   renderParameter (parameter) {
-    return (<tr key={parameter.name}>
-      <td >
-        <span className='label label-default' >{parameter.in}</span>
-      </td>
-      <td className='col-md-2' ><span >{parameter.name}</span></td>
-      <td className='col-md-8' >
-        <span className='doc-w-param-value' >{parameter.description}</span>
-      </td>
-      <td className='col-md-2' >
-        <span className='doc-w-param-datatype' >{this.renderType(parameter)}
-        </span>
-      </td>
-    </tr>)
+    return (
+      <tr key={parameter.name}>
+        <td className='col-md-4'>
+          <span className='label label-default'>{parameter.in}</span>
+          <span> {parameter.name}</span></td>
+        <td className='col-md-6' >
+          <span className='doc-w-param-value'>{this.renderParameterDescription(parameter.description)}</span>
+          {parameter.example && (
+            <code>{JSON.stringify(parameter.example)}</code>
+          )}
+        </td>
+        <td className='col-md-2' >
+          <span className='doc-w-param-datatype' >{this.renderType(parameter)}
+          </span>
+        </td>
+      </tr>
+    )
   }
 
   renderType (parameter) {
@@ -47,7 +58,7 @@ class SpecWidgetTab extends Component {
             <p>{this.props.operation.spec.summary}</p>
             <p className='text-muted' >{this.props.operation.spec.description}</p>
             <div >
-              <div >
+              <div>
                 <h4 >Parameters</h4>
                 <table className='table table-striped' >
                   <tbody className='operation-params' >
