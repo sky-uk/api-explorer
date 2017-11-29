37a885448223cb0ec7c907f384e0fe378809930f
Carlos Guedes <carlos.guedes@sky.uk>
2017-11-29 17:21:38 +0000

    Adds the CurlGeneratorTab to the sample-petstore

--- samples/sample-petstore/src/index.js
+++ samples/sample-petstore/src/index.js
@@ -1,7 +1,11 @@
 import './index.css'
 
 import APIExplorer from 'apiexplorer'
+import { CurlGeneratorTab } from 'apiexplorer-components'
 
 APIExplorer
-  .addAPI('petstore', 'swagger2', 'http://petstore.swagger.io/v2/swagger.json')
+  .addAPI('petstore', 'swagger2', 'http://petstore.swagger.io/v2/swagger.json', c => {
+    c.listOperationsAtWelcome(true)
+  })
+  .addWidgetTab('CURL', CurlGeneratorTab)
   .start('app')
