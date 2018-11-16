7fe04ea25ee74558bea2d9d6a0ce3f55e5ea5de9
Almeida, Daniel (Developer) <daniel.almeida@sky.uk>
2018-11-16 18:36:46 +0000

    strategic change to support swagger2 request executor

--- packages/apiexplorer/src/infrastructure/loaders/swagger2Loader.js
+++ packages/apiexplorer/src/infrastructure/loaders/swagger2Loader.js
@@ -1,31 +1,32 @@
-import URI from 'urijs'
+import SwaggerParser from 'swagger-parser'
 
 export function swagger2Loader (config, { onLoadProgress, onNewAPI, onNewOperation, onNewDefinition, onLoadCompleted, onLoadError }) {
   const url = config.url.getUrl()
   onLoadProgress(`Loading API Spec from ${url}`)
 
-  return fetch(url, { credentials: 'include' })
-    .then(response => response.json())
-    .then(apiSpec => {
-      let newApiSpec = apiSpec
-      let defaultHost = new URI(config.url.url).host()
+  return SwaggerParser.validate(url)
+    .then(function (api) {
+      let newApi = api
+      let defaultHost = window.location.origin
 
-      newApiSpec = config.interceptor({ friendlyName: config.friendlyName, url: config.url }, apiSpec)
-
-      swagger2JsonLoader(newApiSpec, config.friendlyName, config.slug, defaultHost, { onLoadProgress, onNewAPI, onNewOperation, onNewDefinition, onLoadCompleted, onLoadError })
+      newApi = config.interceptor({ friendlyName: config.friendlyName, url: config.url }, api)
+      swagger2JsonLoader(newApi, config.friendlyName, config.slug, defaultHost, { onLoadProgress, onNewAPI, onNewOperation, onLoadCompleted, onLoadError })
+    })
+    .catch(function (err) {
+      onLoadError(err)
     })
 }
 
-export function swagger2JsonLoader (apiSpec, friendlyName, slug, defaultHost, { onLoadProgress, onNewAPI, onNewOperation, onNewDefinition, onLoadCompleted, onLoadError }) {
+export function swagger2JsonLoader (apiSpec, friendlyName, slug, defaultHost, { onLoadProgress, onNewAPI, onNewOperation, onLoadCompleted, onLoadError }) {
   onLoadProgress(`API Spec received with success`)
   onLoadProgress(`Starting API parsing`)
 
   // defaults
   apiSpec = Object.assign({
     definitions: [],
-    basePath: '/',
-    host: defaultHost
+    basePath: '/'
   }, apiSpec)
+  apiSpec.host = apiSpec.schemes[0] + '://' + apiSpec.host ||  defaultHost
 
   onNewAPI(apiSpec)
 
@@ -48,15 +49,6 @@ export function swagger2JsonLoader (apiSpec, friendlyName, slug, defaultHost, { 
           onLoadProgress(`Processing operation ${id}`)
           onNewOperation(operationSpec)
         })
-    })
-
-  Object.keys(apiSpec.definitions)
-    .forEach(definitionName => {
-      const definition = apiSpec.definitions[definitionName]
-      const id = `${slug}-${definitionName.toLocaleLowerCase()}`
-      const definitionSpec = {key: `#/definitions/${definitionName}`, name: `${definitionName}`, definition}
-      onLoadProgress(`Processing definition ${id}`)
-      onNewDefinition(definitionSpec)
     })
 
   onLoadProgress(`Loading completed`)
