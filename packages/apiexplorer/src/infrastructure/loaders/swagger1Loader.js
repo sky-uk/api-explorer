bc1596ad493fe8c7b8725cfe8e92d45873dd5e43
Diogo Silva <diogo.silva2@sky.uk>
2018-11-29 09:47:38 +0000

    Removed api-spec-converter, restored previous swagger1Loader

--- packages/apiexplorer/src/infrastructure/loaders/swagger1Loader.js
+++ packages/apiexplorer/src/infrastructure/loaders/swagger1Loader.js
@@ -1,27 +1,66 @@
 /* global fetch */
 
+import Enumerable from 'linq'
 import { swagger2SpecLoader } from './swagger2Loader'
-import SwaggerParser from 'swagger-parser'
-import Converter from 'api-spec-converter/dist/api-spec-converter'
+import SwaggerConverter from 'swagger-converter'
+import URI from 'urijs'
+
+function executeInterceptor (config, apiSpec) {
+  if (config.interceptor) {
+    return config.interceptor({ friendlyName: config.friendlyName, url: config.url }, apiSpec)
+  }
+  return apiSpec
+}
+
+function executeFetch (req, callback) {
+  req.onLoadProgress(`Starting getting new api path from '${req.url}'`)
+
+  fetch(req.url, { credentials: 'include' })
+    .then(response => response.json())
+    .then(apiSpec => {
+      req.onLoadProgress(`New api definition from path '${decodeURIComponent(req.url)}' completed`)
+      callback(null, {path: req.path, result: executeInterceptor(req.config, apiSpec)})
+    })
+    .catch(ex => callback(new Error(`Error loading url ${req.url}: ${ex}`)))
+}
 
 export default function swagger1Loader (config, { onLoadProgress, onNewAPI, onNewOperation, onNewDefinition, onLoadCompleted, onLoadError }) {
   const url = config.url.getUrl()
   onLoadProgress(`Loading API Swagger 1.0 Spec from ${url}`)
 
-  return Converter.convert({
-    from: 'swagger_1',
-    to: 'swagger_2',
-    source: url
-  }).then(function (converted) {
-    SwaggerParser.validate(converted.spec).then(function (api) {
-      let newApi = api
-      let defaultHost = window.location.origin
-      newApi = config.interceptor({ friendlyName: config.friendlyName, url: config.url }, api)
-      swagger2SpecLoader(newApi, config.friendlyName, config.slug, defaultHost, { onLoadProgress, onNewAPI, onNewOperation, onLoadCompleted, onLoadError })
-    }).catch(function (err) {
-      onLoadError(err)
+  const async = require('async')
+
+  return fetch(url, { credentials: 'include' })
+    .then(response => response.json())
+    .then(apiSpec => {
+      let newApiSpec = executeInterceptor(config, apiSpec)
+      let defaultHost = new URI(config.url.url).host()
+
+      onLoadProgress(`Loading content from '${url}' completed`)
+      const apis = Enumerable
+        .from(newApiSpec.apis)
+        .select(api => {
+          return { path: api.path, url: config.url.resolveChildUrl(`${apiSpec.basePath}${api.path}`), onLoadProgress: onLoadProgress, config }
+        })
+        .toArray()
+
+      async.map(apis, executeFetch, (err, result) => {
+        if (err) {
+          onLoadError(`Error loading Swagger 1.x apis: ${err}`)
+          console.error(err)
+        } else {
+          onLoadProgress(`Loading of Swagger spec 1.x completed.`)
+
+          let apiDeclarations = {}
+          result.forEach(element => { apiDeclarations[element.path] = element.result })
+
+          onLoadProgress('Started to convert Swagger 1.x to Swagger 2.0....')
+          const swagger2Document = SwaggerConverter.convert(apiSpec, apiDeclarations)
+          onLoadProgress('Convertion from Swagger 1.x to Swagger 2.0 completed!')
+          swagger2Document.info.title = swagger2Document.info.title === 'Title was not specified' ? 'API' : swagger2Document.info.title
+
+          swagger2SpecLoader(swagger2Document, config.friendlyName, config.slug, defaultHost, { onLoadProgress, onNewAPI, onNewOperation, onNewDefinition, onLoadCompleted, onLoadError })
+        }
+      })
     })
-  }).catch(function (err) {
-    onLoadError(err)
-  })
 }
